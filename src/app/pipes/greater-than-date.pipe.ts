349d27115681768b1ddc41cfd33c5b3d41b9dced
Tommy McConnell <tommy_mcconnell@hotmail.com>
2018-06-20 20:43:04 -0400

    set time to midnight

--- src/app/pipes/greater-than-date.pipe.ts
+++ src/app/pipes/greater-than-date.pipe.ts
@@ -7,6 +7,7 @@ export class GreaterThanDatePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     const date = new Date();
+    date.setHours(0,0,0,0);
     return value.filter((val, index) => {
       return new Date(val.date) >= date;
     })
